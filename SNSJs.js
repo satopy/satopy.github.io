5e97b325dcaf9cac4f8f5e86f3130e54cb9566db
Satoki Higashi <satosato1862@gmail.com>
2017-09-19 15:19:12 +0900

    Add files via upload

---
+++ SNSJs.js
@@ -0,0 +1,21 @@
+//okpt("CSS Particle Effects");
+function initparticles() {
+    bubbles();
+    hearts();
+    lines();
+    confetti();
+    fire();
+    sunbeams();
+ }
+ 
+ /*The measurements are ... whack (so to say), for more general text usage I would generate different sized particles for the size of text; consider this pen a POC*/
+ 
+ function bubbles() {
+    $.each($(".particletext.bubbles"), function(){
+       var bubblecount = ($(this).width()/50)*10;
+       for(var i = 0; i <= bubblecount; i++) {
+          var size = ($.rnd(40,80)/10);
+          $(this).append('<span class="particle" style="top:' + $.rnd(20,80) + '%; left:' + $.rnd(0,95) + '%;width:' + size + 'px; height:' + size + 'px;animation-delay: ' + ($.rnd(0,30)/10) + 's;"></span>');
+       }
+    });
+ }
